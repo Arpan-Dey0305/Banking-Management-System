edf33f9065285982c71803f7922e62f6d4561485
Arpan Dey <adey011003@gmail.com>
2025-08-03 10:50:15 +0530

    banking_project.sql

--- banking_project.sql
+++ banking_project.sql
@@ -1,16 +1,14 @@
 -- 1. Create the database
-CREATE DATABASE IF NOT EXISTS project;
+CREATE DATABASE IF NOT EXISTS DB_NAME;
+
+--Use th database
 USE project;
 
 -- 2. Create the BANK table
-CREATE TABLE IF NOT EXISTS BANK (
+CREATE TABLE IF NOT EXISTS TABLE_NAME (
     ACCNO INT PRIMARY KEY,
     NAME VARCHAR(50),
     BALANCE INT,
     MOB CHAR(10)
 );
 
--- 3. Insert initial data
-INSERT INTO BANK (ACCNO, NAME, BALANCE, MOB) VALUES
-(101, 'Arpan Dey', 4500, '8710042392'),
-(102, 'Chandana Dey', 3000, '8617092123');
